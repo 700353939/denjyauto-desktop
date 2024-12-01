93c5b3091ea83db95678926299e6c5ba960690ce
rosica.msp <internet2222@abv.bg>
2024-12-01 08:31:57 +0200

    fix: add onetoone client to user

--- denjyauto/accounts/signals.py
+++ denjyauto/accounts/signals.py
@@ -7,9 +7,9 @@ from denjyauto.clients.models import Client
 @receiver(post_save, sender=Client)
 def create_user_for_client(sender, instance, created, **kwargs):
 
-        if instance.points >= 10:
+        if instance.points >= 10 and instance.user is None:
             user = CustomUser.objects.create_user(
-                username=instance.username or f'{instance.name[:3].lower()}{instance.pk}',
+                username=instance.username or f'{instance.name[:5].lower()}{instance.pk}',
                 email=instance.email or "default@email.com",
                 password="defaultpassword123"
             )
@@ -19,3 +19,5 @@ def create_user_for_client(sender, instance, created, **kwargs):
             clients_group, created = Group.objects.get_or_create(name='Clients')
 
             user.groups.add(clients_group)
+
+            Client.objects.filter(pk=instance.pk).update(user=user)
